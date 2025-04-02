a13f1ed4b856cd9f7931716e7794e018f48ff917
trilvid <trilztech34@gmail.com>
2025-04-02 13:04:42 +0100

    first commit

--- src/components/Forexcrossmaps.jsx
+++ src/components/Forexcrossmaps.jsx
@@ -28,7 +28,7 @@ const Forexcrossmaps = () => {
   }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={containerRef}>
+    <div className="tradingview-widget-container" ref={containerRef} data-aos="fade-up">
       <div className="tradingview-widget-container__widget"></div>
       <div className="tradingview-widget-copyright">
       </div>
