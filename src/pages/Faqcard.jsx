705fd60b278b57167a905de0664f8e5c1cc6572a
trilvid <trilztech34@gmail.com>
2025-04-04 12:25:13 +0100

    first commit

--- src/pages/Faqcard.jsx
+++ src/pages/Faqcard.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import {ImStack} from 'react-icons/im'
-import {SiReactos} from 'react-icons/si'
-import {TbCertificate,TbChartDots2} from 'react-icons/tb'
-import {TfiPieChart} from 'react-icons/tfi'
-import {BsBarChartLine} from 'react-icons/bs'
-import {BiNetworkChart} from 'react-icons/bi'
-import {GiChart} from 'react-icons/gi'
-import { MdOutlineAddchart } from 'react-icons/md'
 import CountUp from 'react-countup'
 const Faqcard = () => {
   return (
-    <div className='why-choose-section'>
+    <div className='why-choose-section stat-section'>
       <div className="why-choose-us-img-container">
         <div className="videoframe-text-container" data-aos="fade-up">
             <h1>Market  <span className="highlight">rates</span></h1>
             <p>Grab an overview of global markets including price changes, open, high, low, and close values for selected instruments.</p>
         </div>
         <img src="/web-phone.png" alt="" className="mockup" data-aos="fade-up"/>
-        </div>
+      </div>
       <div className="why-choose-us-card-container">
         <div className="why-choose-us-text-container">
             <div className="header" data-aos="fade-up">
