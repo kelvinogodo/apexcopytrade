66b0d690f9fb84bbc2bae7ca67ab78b880ce0c44
trilvid <trilztech34@gmail.com>
2025-04-14 01:28:18 +0100

    first commit

--- src/components/userdashboardTraders/UserdashboardTraders.jsx
+++ src/components/userdashboardTraders/UserdashboardTraders.jsx
@@ -13,6 +13,7 @@ import { IoIosArrowBack } from "react-icons/io";
 const UserdashboardTraders = ({route}) => {
   const [loader, setLoader] = useState(false)
   const [showTrader, setShowTrader] = useState(false)
+  const [activeTrader,setActiveTrader]= useState({})
   
       const [userData, setUserData] = useState()
       const navigate = useNavigate()
@@ -57,7 +58,57 @@ const UserdashboardTraders = ({route}) => {
         };
       
         getData();
-         }, [navigate, route]);
+      }, [navigate, route]);
+  const traders = [
+    {
+      id: 1,
+      firstname: 'bobby',
+      lastname: 'jones',
+      winRate: '85%',
+      averageReturn: '25%',
+      followers: '702',
+      rrRatio: '1:6',
+      nationality: 'united states of America',
+      minimumCapital: 7000,
+      traderImage:'black3.jpg'
+    },
+    {
+      id: 2,
+      firstname: 'jerome',
+      lastname: 'bush',
+      winRate: '95%',
+      averageReturn: '85%',
+      followers: '1,002',
+      rrRatio: '1:7',
+      nationality: 'united states of America',
+      minimumCapital: 10000,
+      traderImage:'blackInvestor.jpeg'
+    },
+    {
+      id: 3,
+      firstname: 'ada',
+      lastname: 'vekury',
+      winRate: '95%',
+      averageReturn: '85%',
+      followers: '1,290',
+      rrRatio: '1:5',
+      nationality: 'canada',
+      minimumCapital: 10000,
+      traderImage:'coporate-woman.jpg'
+    },
+    {
+      id: 4,
+      firstname: 'john',
+      lastname: 'white',
+      winRate: '95%',
+      averageReturn: '85%',
+      followers: '20,200',
+      rrRatio: '1:4',
+      nationality: 'united kingdom',
+      minimumCapital: 10000,
+      traderImage:'fortune-vieyra-QIMjYJSFoXM-unsplash.jpg'
+    },
+  ]
   return (
     <main className='homewrapper'>
          {
@@ -84,7 +135,7 @@ const UserdashboardTraders = ({route}) => {
               </div>
             </div>
         {
-          showTrader &&
+          showTrader && activeTrader ?
           <div className="trader-profile-page">
               <div className="trader-page-close-btn" onClick={()=> setShowTrader(false)}>
                 <IoIosArrowBack />
@@ -93,42 +144,38 @@ const UserdashboardTraders = ({route}) => {
                 <div className="trader-profile-card">
                 <div className="trader-card-header">
                   <div className="trader-card-image-container">
-                  <img src="/black3.jpg" alt="" className='trader-card-image' />
+                  <img src={`/${activeTrader.traderImage}`} alt="" className='trader-card-image' />
                   </div>
                   <div className="trader-card-text-container">
-                    <h3 className="trader-name">John Doe</h3>
-                    <p className="trader-description">nec ligula.</p>
+                    <h3 className="trader-name">{activeTrader.firstname}</h3>
+                    <p className="trader-description">{activeTrader.lastname}</p>
                   </div>
                 </div>
                 <div className="trader-perfomance-container">
                   <div className="trader-performance">
                     <div className="trader-performance-item">
                       <p className="performance-label">Win Rate</p>
-                      <p className="performance-value"><MdCandlestickChart /> 85%</p>
+                      <p className="performance-value"><MdCandlestickChart /> {activeTrader.winRate}</p>
                     </div>
                     <div className="trader-performance-item">
                       <p className="performance-label">Average Return</p>
-                      <p className="performance-value"><MdOutlineShowChart /> 12%</p>
-                    </div>
-                    <div className="trader-performance-item">
-                      <p className="performance-label">profit Ratio</p>
-                      <p className="performance-value">98%</p>
+                      <p className="performance-value"><MdOutlineShowChart /> {activeTrader.averageReturn}</p>
                     </div>
                     <div className="trader-performance-item">
                       <p className="performance-label">followers </p>
-                      <p className="performance-value"> 503</p>
+                      <p className="performance-value"> {activeTrader.followers}</p>
                     </div>
                     <div className="trader-performance-item">
                       <p className="performance-label">minimum Risk/Reward Ratio </p>
-                      <p className="performance-value">1:5</p>
+                      <p className="performance-value">{activeTrader.rrRatio}</p>
                     </div>
                     <div className="trader-performance-item">
                       <p className="performance-label">nationality </p>
-                      <p className="performance-value">japanese</p>
+                      <p className="performance-value">{activeTrader.nationality}</p>
                     </div>
                     <div className="trader-performance-item">
                       <p className="performance-label">minimum Trading Capital </p>
-                      <p className="performance-value">$5000</p>
+                      <p className="performance-value">{activeTrader.minimumCapital}</p>
                     </div>
                   </div>
                   <div className="trader-performance-btn-container">
@@ -137,7 +184,7 @@ const UserdashboardTraders = ({route}) => {
                 </div>
               </div>
               </div>
-            </div>
+            </div> : ''
         }
         {
           !showTrader && 
@@ -155,60 +202,51 @@ const UserdashboardTraders = ({route}) => {
               </div>
             </div>
             <div className="traders-card-container">
-              <div className="traders-card">
-                <div className="trader-card-header">
-                  <div className="trader-card-image-container">
-                  <img src="/black3.jpg" alt="" className='trader-card-image' />
-                  </div>
-                  <div className="trader-card-text-container">
-                    <h3 className="trader-name">John Doe</h3>
-                    <p className="trader-description">nec ligula.</p>
-                  </div>
-                </div>
-                <div className="trader-perfomance-container">
-                  <div className="trader-performance">
-                    <div className="trader-performance-item">
-                      <p className="performance-label">Win Rate</p>
-                      <p className="performance-value"><MdCandlestickChart /> 85%</p>
-                    </div>
-                    <div className="trader-performance-item">
-                      <p className="performance-label">Average Return</p>
-                      <p className="performance-value"><MdOutlineShowChart /> 12%</p>
-                    </div>
-                  </div>
-                  <div className="trader-performance-btn-container">
-                    <button className='trader-card-btn' onClick={()=> setShowTrader(true)}>view profile</button>
-                    <button className='trader-card-btn'>copy trade</button>
-                  </div>
-                </div>
-              </div>
-              <div className="traders-card">
-                <div className="trader-card-header">
-                  <div className="trader-card-image-container">
-                  <img src="/gregory-gill-4Bf5LNEPqZ0-unsplash.jpg" alt="" className='trader-card-image' />
-                  </div>
-                  <div className="trader-card-text-container">
-                    <h3 className="trader-name">John Doe</h3>
-                    <p className="trader-description">nec ligula.</p>
-                  </div>
-                </div>
-                <div className="trader-perfomance-container">
-                  <div className="trader-performance">
-                    <div className="trader-performance-item">
-                      <p className="performance-label">Win Rate</p>
-                      <p className="performance-value"><MdOutlineShowChart />85%</p>
-                    </div>
-                    <div className="trader-performance-item">
-                      <p className="performance-label">Average Return</p>
-                      <p className="performance-value"><MdCandlestickChart /> 12%</p>
-                    </div>
-                  </div>
-                  <div className="trader-performance-btn-container">
-                    <button className='trader-card-btn' onClick={()=> setShowTrader(true)}>view profile</button>
-                    <button className='trader-card-btn'>copy trades</button>
-                  </div>
-                </div>
-              </div>
+                { 
+                  traders.map(trader => 
+                    <div className="traders-card">
+                        <div className="trader-card-header">
+                          <div className="trader-card-image-container">
+                          <img src={`/${trader.traderImage}`} alt="" className='trader-card-image' />
+                          </div>
+                          <div className="trader-card-text-container">
+                            <h3 className="trader-name">{trader.firstname}</h3>
+                            <p className="trader-description">{trader.lastname}</p>
+                          </div>
+                        </div>
+                        <div className="trader-perfomance-container">
+                          <div className="trader-performance">
+                            <div className="trader-performance-item">
+                              <p className="performance-label">Win Rate</p>
+                              <p className="performance-value"><MdCandlestickChart /> {trader.winRate}</p>
+                            </div>
+                            <div className="trader-performance-item">
+                              <p className="performance-label">Average Return</p>
+                              <p className="performance-value"><MdOutlineShowChart /> {trader.averageReturn}</p>
+                            </div>
+                          </div>
+                          <div className="trader-performance-btn-container">
+                          <button className='trader-card-btn' onClick={() => {
+                            setActiveTrader(
+                            {
+                              firstname: trader.firstname,
+                              lastname: trader.lastname,
+                              winRate: trader.winRate,
+                              averageReturn: trader.averageReturn,
+                              followers: trader.followers,
+                              rrRatio: trader.rrRatio,
+                              nationality: trader.nationality,
+                              minimumCapital: trader.minimumCapital,
+                              traderImage:trader.traderImage
+                              })
+                            setShowTrader(true)
+                          }}>view profile</button>
+                          <button className='trader-card-btn' >copy trade</button>
+                          </div>
+                        </div>
+                      </div>
+                  )
+              }
               
             </div>
           </div>
